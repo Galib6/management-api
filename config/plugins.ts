7732cc98d0758478580b789baf9bf0635da38ae9
Al Galib <asadullahalgalib81@gmail.com>
2024-03-06 17:51:58 +0600

    email plug installed

--- config/plugins.ts
+++ config/plugins.ts
@@ -13,4 +13,49 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT", 587),
+        // ignoreTLS: true,
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: "algalib1001@gmail.com",
+        defaultReplyTo: "algalib1001@gmail.com",
+      },
+    },
+  },
+  scheduler: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        "api::page.page": {},
+      },
+    },
+  },
+  "entity-relationship-chart": {
+    enabled: true,
+    config: {
+      // By default all contentTypes and components are included.
+      // To exlclude strapi's internal models, use:
+      exclude: [
+        "strapi::core-store",
+        "webhook",
+        "admin::permission",
+        "admin::user",
+        "admin::role",
+        "admin::api-token",
+        "plugin::upload.file",
+        "plugin::i18n.locale",
+        "plugin::users-permissions.permission",
+        "plugin::users-permissions.role",
+      ],
+    },
+  },
 });
